286f88d30dab993e2f6de534caca93a52ffea896
Scrubbeh <danielwangusi@gmail.com>
2025-01-25 22:43:26 +0000

    UI Soundbank

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,64 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID MENUITEM1 = 371673518U;
+        static const AkUniqueID MENUITEM2 = 371673517U;
+        static const AkUniqueID MENUITEM3 = 371673516U;
+        static const AkUniqueID MENUITEM4 = 371673515U;
+        static const AkUniqueID PAUSE = 3092587493U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID RESUME = 953277036U;
+        static const AkUniqueID START_GAME = 1114964412U;
+        static const AkUniqueID STOP_MUSIC = 2837384057U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC
+        {
+            static const AkUniqueID GROUP = 3991942870U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GAMEPLAY = 89505537U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TITLE = 3705726509U;
+            } // namespace STATE
+        } // namespace MUSIC
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace UISELECTION
+        {
+            static const AkUniqueID GROUP = 2454203527U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID PLAY = 1256202815U;
+                static const AkUniqueID U1 = 1551306223U;
+                static const AkUniqueID U2 = 1551306220U;
+                static const AkUniqueID U3 = 1551306221U;
+                static const AkUniqueID U4 = 1551306218U;
+            } // namespace SWITCH
+        } // namespace UISELECTION
+
+    } // namespace SWITCHES
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID GAME_START = 733168346U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
