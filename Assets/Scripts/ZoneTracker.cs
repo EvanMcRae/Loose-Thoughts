9eff06fe4e974b1038eff8038d11a19ee6e0f278
RJWMD <113476572+RJWMD@users.noreply.github.com>
2025-01-24 21:30:19 -0500

    Notes for zone tracker and obstacle script

---
+++ Assets/Scripts/ZoneTracker.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+public class ZoneTracker : MonoBehaviour
+{
+
+    public List<Obstacle> spawnedObstacles = new List<Obstacle>();
+
+    //Set Order of zones:
+    //They will have a transition picture between them
+    //Should order be random or pre-determined?
+
+    //List of zones:
+    //happy
+    //neutral
+    //angry
+    //depressed/sad
+    //scared
+    //anxiety
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
