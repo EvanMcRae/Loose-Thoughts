b161c5fab04d2fd2908a4f55f78ed58582db3e26
Aidan-2020 <69286745+Aidan-2020@users.noreply.github.com>
2025-01-25 12:30:59 -0500

    added a simple flying enemy obstacle

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    
+    public float speed = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public virtual void Start()
