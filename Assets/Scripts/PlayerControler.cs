622ccdc02b0f9f71929a904bd3181d513584eb4f
Jacob Pickholtz <jacobspz@aol.com>
2025-01-25 13:58:52 -0500

    Bounded player to camera, and added box collider
    Also made the camera orthographic instead of based on perspective. This should make calculating the screen space much easier

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -8,11 +8,25 @@ public class PlayerControler : MonoBehaviour
 
     //determines how fast the player returns to rests
     float brakingSpeed = 1.1f;
+
+    //bounds for where player can move (most likely camera)
+    private float xMin, xMax;
+    private float yMin, yMax;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        var spriteSize = GetComponent<SpriteRenderer>().bounds.size.x * .5f;
+
+        var cam = Camera.main;// Camera component to get their size, if this change in runtime make sure to update values
+        var camHeight = cam.orthographicSize;
+        var camWidth = cam.orthographicSize * cam.aspect;
+
+        yMin = -camHeight + spriteSize; // lower bound
+        yMax = camHeight - spriteSize; // upper bound
         
+        xMin = -camWidth + spriteSize; // left bound
+        xMax = camWidth - spriteSize; // right bound 
     }
 
     void FixedUpdate()
@@ -20,8 +34,17 @@ public class PlayerControler : MonoBehaviour
        // float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        var xValidPosition = Mathf.Clamp(transform.position.x, xMin, xMax);
+        var yValidPosition = Mathf.Clamp(transform.position.y, yMin, yMax);
+
+        transform.position = new Vector3(transform.position.x, yValidPosition, 0f);
+
+        //if at the edge of the screen and trying to move further, halt velocity
+        if((transform.position.y == yMin || transform.position.y == yMax) && rb.linearVelocity != Vector2.zero){
+            rb.linearVelocity = Vector2.zero;
+        }
         //if no button presssed, stop player's movement
-        if(vertical == 0f){
+        else if(vertical == 0f){
             rb.linearVelocity /= brakingSpeed;
         }
         //if trying to move in opposite direction of movement, add extra force to desired direction
@@ -32,6 +55,7 @@ public class PlayerControler : MonoBehaviour
         {
             rb.AddForceY(vertical * speed);
         }
+        
         
     }
 }
