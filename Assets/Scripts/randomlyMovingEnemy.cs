0be8e6605374d561540f0fbe4ea1b1cb69d7b54a
Aidan-2020 <69286745+Aidan-2020@users.noreply.github.com>
2025-01-25 15:37:36 -0500

    enemy/obstacle behavior
    enemies can track player or just move towards player but miss. Obstacles can follow a pattern or move randomly. Before player is near enemies, they can roam, they can roam either a set pattern or roam around randomly

---
+++ Assets/Scripts/randomlyMovingEnemy.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomlyMovingEnemy : Obstacle
+{
+    public bool canMove;
+    private int targetIndex;
+    public float startWaitTime;
+
+    private float waitTime;
+
+    public GameObject objectThatHasMovePositions;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    new void Start()
+    {
+        canMove = true;
+
+        waitTime = startWaitTime;
+        targetIndex = 0;
+    }
+
+    // Update is called once per frame
+    new void Update()
+    {
+        if (canMove)
+        {
+            Vector2 targetDirection = objectThatHasMovePositions.GetComponent<movePositions>().targetLocations[targetIndex].position - transform.position;
+
+            float RotateSpeed = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, objectThatHasMovePositions.GetComponent<movePositions>().targetLocations[targetIndex].position, speed * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, objectThatHasMovePositions.GetComponent<movePositions>().targetLocations[targetIndex].position) < .05f)
+            {
+                if (waitTime <= 0)
+                {
+                    //targetIndex += 1;
+                    targetIndex = Random.Range(0, objectThatHasMovePositions.GetComponent<movePositions>().targetLocations.Length);
+                    if (targetIndex == objectThatHasMovePositions.GetComponent<movePositions>().targetLocations.Length)
+                    {
+                        targetIndex = 0;
+                    }
+                    StartCoroutine(ResetTime());
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+
+    IEnumerator ResetTime()
+    {
+        yield return new WaitForSeconds(0.5f);
+        waitTime = startWaitTime;
+    }
+}
---
+++ Assets/Scripts/setMovingEnemy.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setMovingEnemy : Obstacle
+{
+    public bool canMove;
+    private int targetIndex;
+
+    //public int boxerNumber;
+
+    public float startWaitTime;
+
+    private float waitTime;
+   // private bool firstWalked = false;
+
+    public GameObject objectThatHasMovePositions;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    new void Start()
+    {
+        canMove = true;
+
+        waitTime = startWaitTime;
+        targetIndex = 0;
+    }
+
+    // Update is called once per frame
+    new void Update()
+    {
+        //if (canMove && !firstWalked)
+        //{
+        //    firstWalked = true;
+        //}
+        if (canMove)
+        {
+            Vector2 targetDirection = objectThatHasMovePositions.GetComponent<movePositions>().targetLocations[targetIndex].position - transform.position;
+
+            float RotateSpeed = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, objectThatHasMovePositions.GetComponent<movePositions>().targetLocations[targetIndex].position, speed * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, objectThatHasMovePositions.GetComponent<movePositions>().targetLocations[targetIndex].position) < .05f)
+            {
+                if (waitTime <= 0)
+                {
+                    targetIndex += 1;
+                    if (targetIndex == objectThatHasMovePositions.GetComponent<movePositions>().targetLocations.Length)
+                    {
+                        targetIndex = 0;
+                    }
+                    StartCoroutine(ResetTime());
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+    IEnumerator ResetTime()
+    {
+        yield return new WaitForSeconds(0.5f);
+        waitTime = startWaitTime;
+    }
+}
